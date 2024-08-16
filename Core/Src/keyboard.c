22ee6d31de936289ae4affa095fad07232631ce9
Challmymind <81416266+Challmymind@users.noreply.github.com>
2024-08-16 21:56:54 +0200

    this time correct files: n-key rollover

--- Core/Src/keyboard.c
+++ Core/Src/keyboard.c
@@ -11,36 +11,27 @@
 #include "usbd_hid.h"
 
 extern USBD_HandleTypeDef hUsbDeviceFS;
-uint8_t _keybord_report[KEYBOARD_REPORT_SIZE] = {0};
 
-uint8_t LAYOUT_2[3][10] = {
-		{
-				0x14, 0x1a, 0x08, 0x15, 0x17, 0x1c, 0x18, 0x0c, 0x12, 0x13
-		},
-		{
-				0x04, 0x16, 0x07, 0x09, 0x0a, 0x0b, 0x0d, 0x0e, 0x0f, 0x00
-		},
-		{
-				0x1d, 0x1b, 0x06, 0x19, 0x05, 0x11, 0x10, 0x00, 0x00, 0x00
-		}
-};
 
-uint8_t LAYOUT_1[3][10] =
+uint_fast8_t LAYOUTS[1][3][10] =
 {
-	{0x14, 0x1a, 0x08, 0x15, 0x17, 0x1c, 0x18, 0x0c, 0x12, 0x13},
-	{0x04, 0x16, 0x07, 0x09, 0x0a, 0x0b, 0x0d, 0x00, 0x0f, 0x28},
-	{0x1d, 0x1b, 0x06, 0x19, 0x05, 0x11, 0x10, 0x2c, 0x00, 0x00}
+
+		{
+				{0x14, 0x1a, 0x08, 0x15, 0x17, 0x1c, 0x18, 0x0c, 0x12, 0x13},
+				{0x04, 0x16, 0x07, 0x09, 0x0a, 0x0b, 0x0d, 0x0e, 0x0f, 0x00},
+				{0x1d, 0x1b, 0x06, 0x19, 0x2c, 0x05, 0x11, 0x10, 0x00, 0x28}
+		}
+
 };
 
-uint8_t _STATUS[3][10] = {
+uint_fast8_t STATUS[3][10] = {
 		{0,0,0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,0,0}
 };
-
-uint8_t _key_count = 0;
-
-uint8_t _current_layout = MAIN_LAYOUT;
+uint_fast8_t report_offset = 2;
+uint_fast8_t current_layout = MAIN_LAYOUT;
+uint8_t keybord_report[KEYBOARD_REPORT_SIZE] = {0};
 
 GPIO_TypeDef* c_ports[] = {
 		COL0_GPIO_Port,
@@ -82,47 +73,87 @@ uint16_t r_pins[] = {
 
 void keybord_loop(){
 
-	int x = 0;
+	while(1){
 
-	while(x < 3){
-		int y = 0;
-		HAL_GPIO_WritePin(r_ports[x], r_pins[x], 1);
-		while(y < 10){
-			if(HAL_GPIO_ReadPin(c_ports[y], c_pins[y])){
-				if(!_STATUS[x][y]){
-//					_keybord_report[2+_key_count] = LAYOUT_1[x][y];
-//					_key_count += 1;
-//					_STATUS[x][y] = _key_count;
+		uint_fast8_t x = 0;
 
-					_keybord_report[2] = LAYOUT_1[x][y];
-					_key_count += 1;
-					_STATUS[x][y] = _key_count;
+		while(x < 3){
+			uint_fast8_t y = 0;
+			HAL_GPIO_WritePin(r_ports[x], r_pins[x], 1);
+			while(y < 10){
+				if(HAL_GPIO_ReadPin(c_ports[y], c_pins[y])){
+					if(!STATUS[x][y]){
+						//					_keybord_report[2+_key_count] = LAYOUT_1[x][y];
+						//					_key_count += 1;
+						//					_STATUS[x][y] = _key_count;
+
+						STATUS[x][y] = report_offset;
+						report_offset++;
+
+						//					keybord_report[2] = LAYOUTS[current_layout][x][y];
+						//					key_count += 1;
+						//					STATUS[x][y] = key_count;
+					}
+
+				}
+				else {
+					if(STATUS[x][y]){
+						//					_keybord_report[2+_STATUS[x][y]-1] = 0;
+						//					uint8_t size = _key_count - _STATUS[x][y];
+						//					memmove(_keybord_report+(2+_STATUS[x][y]-1),_keybord_report+(2+_STATUS[x][y]),size);
+						//					_STATUS[x][y] = 0;
+						//					_key_count -= 1;
+						//
+						//					if(STATUS[x][y] == key_count) keybord_report[2] = 0;
+						//					STATUS[x][y] = 0;
+						//					key_count -= 1;
+
+
+
+						uint_fast8_t m = 0;
+						while(m < 3){
+							uint_fast8_t n = 0;
+							while(n < 10){
+								if(STATUS[m][n] > STATUS[x][y]) STATUS[m][n]--;
+								n++;
+							}
+							m++;
+						}
+						STATUS[x][y] = 0;
+						report_offset--;
+					}
 				}
 
+				y++;
 			}
-			else {
-				if(_STATUS[x][y]){
-//					_keybord_report[2+_STATUS[x][y]-1] = 0;
-//					uint8_t size = _key_count - _STATUS[x][y];
-//					memmove(_keybord_report+(2+_STATUS[x][y]-1),_keybord_report+(2+_STATUS[x][y]),size);
-//					_STATUS[x][y] = 0;
-//					_key_count -= 1;
-
-					if(_STATUS[x][y] == _key_count) _keybord_report[2] = 0;
-					_STATUS[x][y] = 0;
-					_key_count -= 1;
-				}
-			}
-
-			y += 1;
+			HAL_GPIO_WritePin(r_ports[x], r_pins[x], 0);
+			x++;
 		}
-		HAL_GPIO_WritePin(r_ports[x], r_pins[x], 0);
-		x += 1;
+
+		uint_fast8_t m = 0;
+		memset(keybord_report,0,sizeof(keybord_report));
+//		switch (current_layout){
+//		case MAIN_LAYOUT:
+//			if(STATUS[1][0] || STATUS[0][8]){
+//
+//			}
+//			break;
+//
+//		}
+		while(m < 3){
+			uint_fast8_t n = 0;
+			while(n < 10){
+				if(STATUS[m][n])
+					keybord_report[STATUS[m][n]] = LAYOUTS[current_layout][2-m][9-n];
+				n++;
+			}
+			m++;
+		}
+
+		USBD_HID_SendReport(&hUsbDeviceFS, keybord_report, KEYBOARD_REPORT_SIZE);
+		HAL_Delay(10);
+
+//	}
 	}
-
-	USBD_HID_SendReport(&hUsbDeviceFS, _keybord_report, KEYBOARD_REPORT_SIZE);
-	HAL_Delay(10);
-
-
 }
 
